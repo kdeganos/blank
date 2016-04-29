d4a952890db2acb5e08d677c800f8d6def13a4d5
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-04-29 08:27:45 -0700

    Updated App.java

--- src/main/java/Blank.java
+++ src/main/java/Blank.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 public class Blank{
 
   public Boolean runBlank (Integer year) {
-     // eventually put your code here
-     return false;
+    return false;
    }
  }
 

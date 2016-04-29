c1fadd1f32155c87f8db46a8b38cd6878ca1b047
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-04-29 08:12:53 -0700

    Added AppTest, cleand up comments, renamed some files

--- src/test/java/BlankTest.java
+++ src/test/java/BlankTest.java
@@ -1,56 +1,12 @@
-import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
-import org.fluentlenium.adapter.FluentTest;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import static org.assertj.core.api.Assertions.assertThat;
 
-public class BlankTest extends FluentTest{
 
-  public WebDriver webDriver = new HtmlUnitDriver();
-
-  @Override
- public WebDriver getDefaultDriver() {
-   return webDriver;
- }
-
- @ClassRule
- public static ServerRule server = new ServerRule();
+public class BlankTest {
 
  @Test
- public void rootTest() {
-   goTo("http://localhost:4567/");
-    fill("#???").with("?");
-    submit(".btn");
-    assertThat(pageSource()).contains("what you want to be found related to your method");
+ public void runBlank_CheckifPageIsBlank_true() {
+   Blank myBlank = new Blank();
+   assertEquals(FirstArgument, SecondArgument());
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  @Test
-  public void runBlank_CheckifPageIsBlank_true(){
-    Blank ->MAIN CLASS<- myBlank = new Blank();  ->THIS CREATES A INSTANCE THE MAIN CLASS<-
-    assertEquals(FirstArgument, SecondArgument()) <-METHOD THAT COMPARES 1ST AND 2ND ARGUMENTS->
-  }
 }

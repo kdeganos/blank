01c96f0476de57970a9b42f0cf458b656e2c0bd8
Kevin Deganos <kdeganos-09@sandiego.edu>
2016-05-06 09:52:20 -0700

    fixed staticFileLocation in app java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    // staticfilelocation("/public");
+    // staticFileLocation("/public");
     // String layout = "templates/layout.vtl";
     //
     // get("/", (request, response) -> {
@@ -15,20 +15,6 @@ public class App {
     //   model.put("template", "templates/index.vtl");
     //   return new ModelAndView(model, layout);
     // }, new VelocityTemplateEngine());
-    //
-    // get("/output", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/output.vtl");
-    //
-    //   Blank blankList = new Blank(); /
-    //
-    //   String userInputString = request.queryParams("???");
-    //   Integer userInputNumber = Integer.parseInt(userInput);
-    //
-    //   ArrayList myResults = blankList.runBlank(userInputNumber);
-    //
-    //   model.put("result", myResults);
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+
   }
 }
